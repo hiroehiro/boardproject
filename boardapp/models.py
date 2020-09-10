58307687ca62c9d01c5ee628008a5185c351d94e
github suzunaan <hiroekota114@gmail.com>
2020-09-10 17:13:56 +0900

    modelの作成とImageファイルを扱えるようにする

--- boardapp/models.py
+++ boardapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class BoardModel(models.Model):
+    title=models.CharField(max_length=100)
+    content=models.TextField()
+    author=models.CharField(max_length=100)
+    images=models.ImageField(upload_to="")
+    good=models.IntegerField()
+    read=models.IntegerField() #既読した人数
+    readtext=models.CharField(max_length=200) #既読した人の名前を保管
+    
